23ff396003ac44f4b7a3c3ac88066d640c533048
olgw0331 <olgw0331@gmail.com>
2026-09-08 17:06:30 +0900

    히어로 슬라이드 스와이퍼 내비게이션 버튼 비활성화, 자동재생 버튼

--- js/swHero.js
+++ js/swHero.js
@@ -1,14 +1,32 @@
 const mainVisualBg = document.querySelector(
   ".hero .main-visual .main-visual-bg",
 );
-// console.log(mainVisualBg);
 
+// console.log(mainVisualBg);
+const heroBtns = document.querySelectorAll(".visual-inner .sw-hero-btn");
+// console.log(heroBtns);
+const playStopBtn = document.querySelector(
+  ".sw-hero .pagination .play-stop-btn",
+);
+// console.log(playStopBtn);
+
+// 히어로 슬라이드 버튼 호버 인터랙션
+heroBtns.forEach((btn) => {
+  btn.addEventListener("mouseenter", () => {
+    btn.querySelector(".off").style.opacity = 1;
+  });
+  btn.addEventListener("mouseout", () => {
+    btn.querySelector(".off").style.opacity = 0;
+  });
+});
+
+// 스와이퍼 설정
 const swHero = new Swiper(".sw-hero", {
   speed: 1000,
-  // autoplay: {
-  //   delay: 5000,
-  //   disableOnInteraction: false,
-  // },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: ".swiper-pagination",
     type: "fraction",
@@ -26,9 +44,28 @@ const swHero = new Swiper(".sw-hero", {
 
       const activeSlide = swiper.slides[swiper.activeIndex];
       // console.log(activeSlide);
-      const activeSlideImg = activeSlide.querySelector(".slide-image img");
+      const activeSlideImgUrl =
+        activeSlide.querySelector(".slide-image img").src;
       // console.log(activeSlideImg.src);
       mainVisualBg.style.backgroundImage = `url(${activeSlideImg.src})`;
     },
   },
+});
+
+// 재생, 정지 버튼
+playStopBtn.addEventListener("click", () => {
+  // console.log("슬라이드 자동재생 정지");
+
+  const isRunning = swHero.autoplay.running;
+  // console.log(isRunning);
+
+  if (isRunning) {
+    swHero.autoplay.stop();
+    console.log("자동재생 정지");
+    playStopBtn.style.backgroundImage = `url(assets/icons/slide_play.png)`;
+  } else {
+    swHero.autoplay.start();
+    console.log("자동재생 시작");
+    playStopBtn.style.backgroundImage = `url(assets/icons/slide_stop.png)`;
+  }
 });
